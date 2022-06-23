b2084c137ac35f46308b53bb02ed25cda67c44d3
Malsagov <rus.malsagov177@mail.ru>
2022-06-23 14:21:38 +0300

    Сделал модель status

--- controllers/students.controller.js
+++ controllers/students.controller.js
@@ -20,7 +20,83 @@ module.exports.studentController = {
   },
   getStudentReview: async (req, res) => {
     try {
-      res.json(await Student.findById(req.params.userId).populate("review"));
+      res.json(
+        await Student.findById(req.params.userId)
+          .select("review")
+          .populate("review")
+      );
+    } catch (error) {
+      res.json(error.toString(res.json(error.toString())));
+    }
+  },
+  getStudentPaymentNo: async (req, res) => {
+    try {
+      res.json(await Student.find({ payment: 0 }).populate("review status"));
+    } catch (error) {
+      res.json(error.toString());
+    }
+  },
+  getStudentPaymentHalf: async (req, res) => {
+    try {
+      res.json(await Student.find({ payment: 0.5 }).populate("review status"));
+    } catch (error) {
+      res.json(error.toString());
+    }
+  },
+  getStudentPaymentFull: async (req, res) => {
+    try {
+      res.json(await Student.find({ payment: 1 }).populate("review status"));
+    } catch (error) {
+      res.json(error.toString());
+    }
+  },
+  getStudentStatusId: async (req, res) => {
+    try {
+      res.json(
+        await Student.find({ status: req.params.statusId }).populate(
+          "review status"
+        )
+      );
+    } catch (error) {
+      res.json(error.toString());
+    }
+  },
+  postStudent: async (req, res) => {
+    try {
+      const { name, grade, payment, review, status } = req.body;
+      const data = await Student.create({
+        name,
+        grade,
+        payment,
+        review,
+        status,
+      });
+      res.json(`Студент ${data.name} добавлен!`);
+    } catch (error) {
+      res.json(error.toString());
+    }
+  },
+  patchStudentId: async (req, res) => {
+    try {
+      const { name, grade, payment, review, status } = req.body;
+      const { userId } = req.params;
+      const data = await Student.findByIdAndUpdate(userId, {
+        name,
+        grade,
+        payment,
+        review,
+        status,
+      });
+      res.json(`Студент ${data.name} изменен!`);
+    } catch (error) {
+      res.json(error.toString());
+    }
+  },
+  deleteStudent: async (req, res) => {
+    try {
+      const { userId } = req.params;
+      await Student.findByIdAndRemove(userId);
+      res.json("Студент удален!");
     } catch (error) {
       res.json(error.toString());
     }
--- routes/students.route.js
+++ routes/students.route.js
@@ -1,30 +1,36 @@
 const { Router } = require("express");
+const { studentController } = require("../controllers/students.controller");
 const router = Router();
 
+//Добавление студента
+router.post('/students', studentController.postStudent);
 
-router.post('/students');
-router.delete('/students/:id');
+// Удаление студента
+router.delete('/students/:userId', studentController.deleteStudent);
+
+// Изменение студента
+router.patch('/students/:userId', studentController.patchStudentId)
 
 // Вывод всех студентов
-router.get("/students");
+router.get("/students", studentController.getStudents);
 
 // Все студенты по groupid
-router.get("/students/grade/:gradeId");
+router.get("/students/grade/:gradeId", studentController.getStudentsGrade);
 
 // Вывод заметок о студенту
-router.get("/students/review/:userId");
+router.get("/students/review/:userId", studentController.getStudentReview);
 
 // Студенты, оплатившие 0%
-router.get("/students/paymentno");
+router.get("/students/paymentno", studentController.getStudentPaymentNo);
 
 // Студенты, оплатившие 50%
-router.get("/students/paymenthalf");
+router.get("/students/paymenthalf", studentController.getStudentPaymentHalf);
 
 // Студенты, оплатившие 100%
-router.get("/students/paymentfull");
+router.get("/students/paymentfull", studentController.getStudentPaymentFull);
 
 // Вывод студентов по statusid
-router.get("/students/:statusId");
+router.get("/students/:statusId", studentController.getStudentStatusId);
 
 
 module.exports = router;
